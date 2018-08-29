e75b9a2b9053b792b218d074da4b18892b3e06f8
Guilherme Deconto <deconto100@gmail.com>
2018-08-29 19:46:19 -0300

    Método ex 1

--- exercicioBolsoMito/src/pucrs/App.java
+++ exercicioBolsoMito/src/pucrs/App.java
@@ -1,10 +1,25 @@
 package pucrs;
 
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
+	public static String Soma (int cor){
+	    if (cor == 1){
+            System.out.println("RED");
+        }else if(cor == 2){
+            System.out.println("Yellow");
+        }else if(cor ==3 ){
+            System.out.println("Blue");
+        }else if(cor ==4){
+            System.out.println("Violet");
+        }else{
+            System.out.println("Cor nao encontrada. Tente novamente mais tarde.");
+        }
+
+    }
+
 
 }
