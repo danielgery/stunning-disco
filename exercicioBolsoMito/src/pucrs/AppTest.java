e5d94df53aaa7da7f9986bc100a8bb522cc8f2fd
Guilherme Deconto <deconto100@gmail.com>
2018-08-29 22:25:35 -0300

    Classe de teste criada

---
+++ exercicioBolsoMito/src/pucrs/AppTest.java
@@ -0,0 +1,24 @@
+package pucrs;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void soma() {
+    }
+
+    @Test
+    public void tvAssinatura() {
+    }
+
+    @Test
+    public void situacao() {
+    }
+
+    @Test
+    public void arquivo() {
+    }
+}
